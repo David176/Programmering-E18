c3a4eef67e498aa14c1ca901d7756da253d98a47
dda <dda@kamstrup.com>
2018-09-27 09:25:26 +0200

    tilføjet de to Person klasser vi arbejdede med i timen

---
+++ Person klasse/Test - Hold 3/Test/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Test
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Testkode - Bruges til at teste vores klasse
+      Person Anders = new Person(false, 15, "Anders", "anders@anders.dk", 15000); //Opretter instans af vores klasse - altså et objekt
+      Person Nanna = new Person(true, 23, "Nanna", "Nanna@nanna.dk", -12000); //Opretter instans af vores klasse - altså et objekt
+      Person LillePer = new Person(5, "LillePer"); //Opretter instans af vores klasse - altså et objekt
+
+      Console.WriteLine("Hello, world!"); //Skriver noget ud i vores Console
+      Console.WriteLine("Hello my name is " + Anders.Name); //Skriver noget ud i vores Console
+      Console.WriteLine("I am " + Anders.Age + " years old"); //Skriver noget ud i vores Console
+      Anders.HaveABirthday(); //Kalder metode
+      Console.WriteLine("I am " + Anders.Age + " years old"); //Skriver noget ud i vores Console
+
+      Console.WriteLine("I have this much money " + Anders.BankAccount); //Skriver noget ud i vores Console
+      Anders.DepositCash(150000); //Kalder metode
+      Console.WriteLine("Now I have this much money " + Anders.BankAccount); //Skriver noget ud i vores Console
+
+      bool answer = Anders.IsPersonFemale(); // Kalder metode og sætter svaret (dvs. det der returneres i en lokal variable 'answer')
+      Console.WriteLine(Anders.Name + " is a female: " + answer); // Bruges det svar vi fik fra IsPersonFemale metoden
+
+      Console.WriteLine("Hi my name is " + Nanna.Name + " I am " + Nanna.Age + " years old"); //Skriver noget ud i vores Console
+
+      Console.WriteLine();
+
+    }
+  }
+
+  public class Person
+  {
+    //Properties
+    public bool IsFemale { get; set; }
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public double BankAccount { get; set; }
+    private double _secretAccount { get; set; }
+
+    //Constructors
+    public Person(bool isFemale, int age, string name, string email, double bankAccount)
+    {
+      IsFemale = isFemale;
+      Age = age;
+      Name = name;
+      Email = email;
+      BankAccount = bankAccount;
+    }
+
+    public Person(int age, string name)
+    {
+      Age = age;
+      Name = name;
+    }
+
+    //Metoder
+    public void HaveABirthday() //void er retur-type, dvs. ingenting
+    {
+      Age++;
+      Console.WriteLine("Yay, I had a birthday");
+    }
+
+    public void DepositCash(double amount) //amount er noget vi får når metoden kaldes
+    {
+      BankAccount += amount;
+      _secretAccount += amount * 0.1;
+    }
+
+
+    public bool IsPersonFemale() //bool er retur-type, dvs. den returnerer true eller false
+    {
+      return IsFemale;
+    }
+  }
+}
---
+++ Person klasse/Test2 - Hold 2/Test2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    { //Testkode - Bruges til at teste vores klasse
+      Person David = new Person("David", 25, false, 1500, true); //Opretter instans af vores klasse - altså et objekt
+      Person Sara = new Person("Sara", 27, true, 2000000, true); //Opretter instans af vores klasse - altså et objekt
+      David.DepositMoney(15000000); //Kalder metode
+      David.Kill(); //Kalder metode
+      Person BabyAnders = new Person("Baby Anders", 0, false, true);  //Opretter instans af vores klasse - altså et objekt
+
+      Console.WriteLine("My name is " + David.Name); //Skriver noget ud i vores Console
+      David.Name = "JohnJohn"; //Sætter Name på David objektet
+      Console.WriteLine("My name is " + David.Name);  //Skriver noget ud i vores Console
+      Console.WriteLine("Age: " + David.Age);  //Skriver noget ud i vores Console
+      David.Age++; //Increaser Age med 1
+      Console.WriteLine("Age: " + David.Age);  //Skriver noget ud i vores Console
+
+      bool answer = David.IsPersonAlive(); // Kalder metode og sætter svaret (dvs. det der returneres i en lokal variable 'answer')
+      Console.WriteLine(David.Name + " is a alive: " + answer); // Bruges det svar vi fik fra IsPersonAlive metoden
+
+      Console.WriteLine();
+
+    }
+  }
+
+  public class Person
+  {
+    //Properties
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public bool IsFemale { get; set; }
+    public double Account { get; set; }
+    public bool IsAlive { get; set; }
+    public bool IsBaby { get; set; }
+    private string _secretName { get; set; }
+
+    //Construtor
+    public Person(string name, int age, bool isFemale, double account, bool isAlive)
+    {
+      Name = name;
+      Age = age;
+      IsFemale = isFemale;
+      Account = account;
+      IsAlive = isAlive;
+    }
+
+    public Person(string name, int age, bool isFemale, bool isAlive) //If no account given it is a baby
+    {
+      Name = name;
+      Age = age;
+      IsFemale = isFemale;
+      IsAlive = isAlive;
+      IsBaby = true;
+    }
+
+    //Metoder
+    public void Kill() //void er retur-type, dvs. ingenting
+    {
+      IsAlive = false;
+    }
+
+    public bool IsPersonAlive() //bool er retur-type, dvs. den returnerer true eller false
+    {
+      return IsAlive;
+    }
+
+    public void DepositMoney(double amount) //amount er noget vi får når metoden kaldes
+    {
+      Account += amount;
+    }
+  }
+
+
+}
